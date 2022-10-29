cad2ff49be7a66ebcaeb4fe625b05f3cb1dea865
Roy <62500486+aazf@users.noreply.github.com>
2022-10-29 16:47:34 +0700

    Update zh.ts

--- packages/qrcode-modal/src/browser/languages/zh.ts
+++ packages/qrcode-modal/src/browser/languages/zh.ts
@@ -3,7 +3,7 @@ import { TextMap } from '../types'
 const zh: TextMap = {
   choose_preferred_wallet: '选择你的钱包',
   connect_mobile_wallet: '连接至移动端钱包',
-  scan_qrcode_with_wallet: '使用Gotabit移动app钱包扫描二维码',
+  scan_qrcode_with_wallet: '使用Gotabit钱包扫描二维码',
   connect: '连接',
   qrcode: '二维码',
   mobile: '移动',
