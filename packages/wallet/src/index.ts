8a23ccccd55a2488e9a6e5a8fb5fd0ff3588d973
marsspecies <763773708@qq.com>
2022-11-01 16:24:21 +0800

    add ledger wallet type and export all wallet

--- packages/wallet/src/index.ts
+++ packages/wallet/src/index.ts
@@ -2,3 +2,4 @@ export * from '@gotabit/wallet-keplr';
 export * from '@gotabit/wallet-local';
 export * from '@gotabit/wallet-walletconnect';
 export * from '@gotabit/wallet-core';
+export * from '@gotabit/wallet-gotabit';
